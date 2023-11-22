153b0bb4b9da74dff173617d8c70efcba02a2a5f
SonnyRu <sonnyr.pro@hotmail.com>
2023-11-22 15:07:19 +0100

    Connexion a la DB

--- Forms/FAccueil.cs
+++ Forms/FAccueil.cs
@@ -67,5 +67,10 @@ namespace GestionMatériels.Forms
             nageur.Show();
             this.Hide();
         }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
