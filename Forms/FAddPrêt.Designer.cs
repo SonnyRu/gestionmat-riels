d2d1bfda9246b2a0691d32229d7537374f5d22a7
SonnyRu <sonnyr.pro@hotmail.com>
2023-11-29 16:15:24 +0100

    Création du formulaire AddPrêt

---
+++ Forms/FAddPrêt.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace GestionMatériels.Forms
+{
+    partial class FAddPrêt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FAddPrêt));
+            SuspendLayout();
+            // 
+            // FAddPrêt
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(92, 51, 127);
+            ClientSize = new Size(643, 455);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FAddPrêt";
+            Text = "Ajouter Prêt";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
